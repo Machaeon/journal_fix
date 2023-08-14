662b63725f107a359a28437131b2dd1b44406e6c
Machaeon <drozhd@gmail.com>
2023-08-14 17:16:38 +0300

    Update custom_mod.sql
    Extend keywords field for regional pagination data

--- custom_mod.sql
+++ custom_mod.sql
@@ -1,2 +1,3 @@
 ALTER TABLE `oc_category_description` ADD `seo_prefix` VARCHAR(255) NOT NULL AFTER `meta_keyword`;
 ALTER TABLE `oc_category_description` ADD `options_title` VARCHAR(255) NOT NULL AFTER `meta_keyword`;
+ALTER TABLE `oc_category_description` CHANGE `meta_keyword` `meta_keyword` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
